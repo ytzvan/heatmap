30c3a89e75cf06a65a64f68a190db9459cd5c28d
Ytzvan Mastino <mastino14@gmail.com>
2015-01-26 18:52:57 -0500

    Fetch data from API, works for now. TODO: Set ENV variables for URL and TOKEN

--- api/controllers/LocationsController.js
+++ api/controllers/LocationsController.js
@@ -5,7 +5,32 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+
 module.exports = {
-	
+
+	fetchData : function(req, res) {
+		var request = require('request');
+		var token = "PUT TOKEN HERE";
+		var options = {
+		    url: 'PUT URL HERE',
+		    headers: {
+		    		'Content-Type': 'application/json',	
+		        'Authorization': 'Bearer ' +token
+		    }
+		};
+
+		function callback(error, response, body) {
+
+		    if (!error && response.statusCode == 200) {
+		        var info = JSON.parse(body);
+		        console.log(info);
+		        console.log(response.statusCode);
+		    } else {
+		    	console.log(response.statusCode);
+		    }
+		}
+
+		request(options, callback);
+	}
 };
 
